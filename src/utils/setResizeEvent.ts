622224c002f7850bcf5e691d9d401ea572bbda13
JusticHentai <1360636180@qq.com>
2022-10-31 18:01:59 +0800

    feat: add debounce

--- src/utils/setResizeEvent.ts
+++ src/utils/setResizeEvent.ts
@@ -1,3 +1,4 @@
+import { debounce } from '@justichentai/math'
 import { InnerOptions } from '../types'
 import update from './update'
 
@@ -5,7 +6,12 @@ import update from './update'
  * 设置 resize 事件
  */
 export default function setResizeEvent(options: InnerOptions) {
+  const myUpdate = debounce({
+    cb: update,
+    duration: 500,
+  })
+
   window.addEventListener('resize', () => {
-    update(options)
+    myUpdate(options)
   })
 }
