54a988d7f97a80a5a6e65e07f855c45310ff331a
JusticHentai <1360636180@qq.com>
2024-08-13 18:11:40 +0800

    版本更新

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,36 @@
-import { DEFAULT_EXTENSIONS } from '@babel/core'
-import { babel } from '@rollup/plugin-babel'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
-import typescript from 'rollup-plugin-typescript2'
 
 export default [
   {
     input: './index.ts',
     output: [
       {
-        file: './dist/my-component.js',
+        file: './dist/utils.js',
         format: 'es',
       },
     ],
     plugins: [
-      // 使得 rollup 支持 commonjs 规范，识别 commonjs 规范的依赖
-      commonjs(),
-      // 配合 commonjs 解析第三方模块
       resolve(),
-      // ts
+      commonjs(),
       typescript(),
-      // babel
-      babel({
-        babelHelpers: 'runtime',
-        // 跳过monorepo里的rollup问题
-        skipPreflightCheck: 'true',
-        //支持 ts
-        extensions: [...DEFAULT_EXTENSIONS, '.ts'],
-        // @babel/plugin-transform-runtime 找不到的坑
-        exclude: /^(.+\/)?node_modules\/.+$/,
+      getBabelOutputPlugin({
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              useBuiltIns: 'usage',
+              corejs: '3.38.0',
+            },
+          ],
+        ],
+        plugins: ['@babel/plugin-transform-runtime'],
       }),
-      // 打包后压缩回车和空格
       terser(),
     ],
   },
@@ -40,7 +38,7 @@ export default [
     input: './index.ts',
     output: [
       {
-        file: './dist/my-component.d.ts',
+        file: './dist/utils.d.ts',
         format: 'es',
       },
     ],
